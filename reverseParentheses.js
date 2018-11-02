7decc7e54212c9602c2bc50e62631519d911208d
Evan Beyerle <ebeyer2007@hotmail.com>
2018-11-01 23:31:49 -0400

    Add files via upload

---
+++ reverseParentheses.js
@@ -0,0 +1,15 @@
+function reverseParentheses(s) {
+    if(s.includes("(")){
+        return reverseParentheses((reverseString(s)));
+    }
+    return s;
+}
+
+function reverseString(s){
+    var regexp = /\(([^()]*)\)/i;
+    var subStr = regexp.exec(s)[1];
+    
+    subStr = subStr.split("").reverse().join("");
+    
+    return s.replace(regexp, subStr);
+}
